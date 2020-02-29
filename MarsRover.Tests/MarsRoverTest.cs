45cc696be5504e96729144adc73f558884d4ef4e
muzaffer.aydin@otelz.com <muzaffer.aydin@otelz.com>
2020-02-29 15:25:13 +0300

    Add project files.

---
+++ MarsRover.Tests/MarsRoverTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarsRover.Tests
+{
+    [TestClass]
+    public class MarsRoverTest
+    {
+        [TestMethod]
+        public void DeployRoversTest()
+        {
+            ILandingSurface landingSurface = new Plateau("5 5");
+            RoverSquad roverSquad = new RoverSquad(landingSurface);
+
+            roverSquad.DeployNewRover("1 2 N", "LMLMLMLMM");
+            roverSquad.DeployNewRover("3 3 E", "MMRMMRMRRM");
+
+            int noRovers = 2;
+            int roverOneIdx = 0;
+            int roverTwoIdx = 1;
+
+            Assert.IsTrue(roverSquad.Count == noRovers);
+
+            IRover roverOne = roverSquad[roverOneIdx];
+            IRover roverTwo = roverSquad[roverTwoIdx];
+
+            Assert.IsNotNull(roverOne);
+            Assert.IsNotNull(roverTwo);
+        }
+
+        [TestMethod]
+        public void DeployRoversAndTestCoordinates()
+        {
+            ILandingSurface landingSurface = new Plateau("5 5");
+            RoverSquad roverSquad = new RoverSquad(landingSurface);
+
+            roverSquad.DeployNewRover("1 2 N", "LMLMLMLMM");
+            roverSquad.DeployNewRover("3 3 E", "MMRMMRMRRM");
+
+            int roverOneIdx = 0;
+            int roverTwoIdx = 1;
+
+            IRover roverOne = roverSquad[roverOneIdx];
+            IRover roverTwo = roverSquad[roverTwoIdx];
+
+            Assert.IsTrue(roverOne.XCoordinate == 1, "RoverOne: X != 1");
+            Assert.IsTrue(roverOne.YCoordinate == 3, "RoverOne: Y != 3");
+            Assert.IsTrue(roverOne.DirectionFacing == "N", "RoverOne: Direction != North");
+
+            Assert.IsTrue(roverTwo.XCoordinate == 5, "RoverTwo: X != 5");
+            Assert.IsTrue(roverTwo.YCoordinate == 1, "RoverTwo: Y != 1");
+            Assert.IsTrue(roverTwo.DirectionFacing == "E", "RoverTwo: Direction != East");
+        }
+    }
+}
---
+++ MarsRover.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MarsRover.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MarsRover.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e38a6bca-2b3a-4144-9633-03ff058ea644")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
